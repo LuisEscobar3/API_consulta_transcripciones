d4efdf4794036d88a64e7a925c04c5a1ea704a7c
1032497498 <lus.escobar@segurosbolivar.com>
2025-12-02 20:59:58 -0500

    ajuste puertos

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.services.main:app", # Ruta corregida a la app FastAPI
         host="0.0.0.0",          # Necesario para que sea accesible desde fuera del contenedor
-        port=8000,
+        port=8080,
         reload=True
     )
